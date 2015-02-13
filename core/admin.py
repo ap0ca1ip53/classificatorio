39741f79f92447aa7d164087fcfc368d42f07a2d
ap0ca1ip53 <jfacardoso@gmail.com>
2015-02-13 13:20:03 -0200

    a.0.1

--- core/admin.py
+++ core/admin.py
@@ -4,7 +4,12 @@ from core.models import Candidato, Cidade
 # Register your models here.
 
 class CandidatoAdmin(admin.ModelAdmin):
-    list_display = ('id', 'pess_Nome', 'pess_CPF','pess_NomePai', 'pess_NomeMae')
+    list_display = ('id', 'pess_Nome', 'pess_CPF','cand_NomePai', 'cand_NomeMae')
+
+class CidadeAdmin(admin.ModelAdmin):
+	ordering = ['cidade_UF', 'cidade_Nome']
+	list_display = ('id', 'cidade_UF', 'cidade_Nome')
+	list_filter = ('cidade_UF',)
 
 admin.site.register(Candidato, CandidatoAdmin)
-admin.site.register(Cidade)
+admin.site.register(Cidade, CidadeAdmin)
--- core/models.py
+++ core/models.py
@@ -6,6 +6,7 @@ from localflavor.br.br_states import STATE_CHOICES
 class Cidade(models.Model):
 	cidade_UF = models.CharField(max_length=2, choices=STATE_CHOICES)
 	cidade_Nome = models.CharField(max_length=40)
+	cidade_CodIBGE = models.IntegerField()
 
 	def __str__(self):
 		return self.cidade_Nome
@@ -16,8 +17,6 @@ class Pessoa(models.Model):
 
 	pess_Nome = models.CharField(max_length=50, blank=True)
 	pess_DtNascimento = models.DateField()
-	pess_NomePai = models.CharField(max_length=50, blank=True)
-	pess_NomeMae = models.CharField(max_length=50, blank=True)
 	pess_Endereco = models.CharField(max_length=50, blank=True)
 	pess_Numero = models.CharField(max_length=10, blank=True)
 	pess_Complemento = models.CharField(max_length=50, blank=True)
@@ -26,9 +25,11 @@ class Pessoa(models.Model):
 	pess_Cidade = models.ForeignKey(Cidade, blank=True)
 	pess_SEXO = models.CharField(max_length=2, choices=CHOICE_SEXO)
 	pess_Email = models.EmailField()
-	pess_FoneResidencial = models.CharField(max_length=11)
-	pess_Celular = models.CharField(max_length=11)
-
+	pess_FoneResidencial = models.CharField(max_length=11, blank=True)
+	pess_Celular = models.CharField(max_length=11, blank=True)
+	pess_RG = models.CharField(max_length=15, blank=True)
+	pess_RGOrgaoExpedidor = models.CharField(max_length=10, blank=True)
+	pess_RGEstado = models.CharField(max_length=2, choices=STATE_CHOICES)
 	pess_CPF = models.CharField(max_length=11, blank=True)
 
 
@@ -37,5 +38,12 @@ class Pessoa(models.Model):
 
 
 class Candidato(Pessoa):
-	nomePai = models.CharField(max_length=50)
-	nomeMae = models.CharField(max_length=50)
+	cand_NomePai = models.CharField(max_length=50, blank=True)
+	cand_NomeMae = models.CharField(max_length=50, blank=True)
+
+
+class Colaborador(Pessoa):
+	colab_SIAPE = models.CharField(max_length=10)
+	colab_Banco = models.CharField(max_length=5)
+	colab_Agencia = models.CharField(max_length=15)
+	colab_Conta = models.CharField(max_length=15)
